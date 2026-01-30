e8f401c32101dc9c5a309fd5ffed9d591c8fef61
IcedTAddict <temourislife100@gmail.com>
2026-01-29 16:54:38 -0800

    Changed motor types for ClimbSubsystem

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.kt
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.kt
@@ -1,17 +1,28 @@
 package frc.robot.subsystems
 
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.SparkBase
+import com.revrobotics.spark.SparkLowLevel
 import com.revrobotics.spark.config.ClosedLoopConfig
 import com.revrobotics.spark.config.SparkMaxConfig
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.Constants
 import frc.robot.Constants.ClimbConstants
 
 
 
 object ClimbSubsystem : SubsystemBase() {
-    private val LeftArm = PWMSparkMax(ClimbConstants.LeftArmID)
-    private val RightArm = PWMSparkMax(ClimbConstants.RightArmID)
+    private val LeftArm =
+        SparkMax(
+            Constants.ClimbConstants.LeftArmID,
+            SparkLowLevel.MotorType.kBrushless
+        )
+    private val RightArm =
+        SparkMax(
+            Constants.ClimbConstants.RightArmID,
+            SparkLowLevel.MotorType.kBrushless
+        )
     init {
         val leftConfig =
             SparkMaxConfig().apply {
