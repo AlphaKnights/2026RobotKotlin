ae481ba4e9717168008675a0f6afba33b8c11d3d
IcedTAddict <temourislife100@gmail.com>
2026-02-02 16:55:14 -0800

    Delivery command done

---
+++ src/main/java/frc/robot/commands/DeliveryCommand.kt
@@ -0,0 +1,33 @@
+package frc.robot.commands.coralmanipulator
+
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.Command
+import frc.robot.Constants
+import frc.robot.subsystems.CoralManipulatorSubsystem
+import frc.robot.subsystems.DeliverySubsystem
+
+class DeliveryCommand : Command() {
+
+    init {
+        // each subsystem used by the command must be passed into the addRequirements() method
+        addRequirements(DeliverySubsystem)
+
+    }
+
+    override fun initialize() {
+        super.initialize()
+    }
+
+    override fun execute() {
+        DeliverySubsystem.forward(Constants.LaunchConstants.LAUNCH_SPEED)
+
+    }
+
+    override fun isFinished(): Boolean {
+        return false
+    }
+
+    override fun end(interrupted: Boolean) {
+        DeliverySubsystem.stop()
+    }
+}
